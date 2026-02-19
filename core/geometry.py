c922c7a09bffff8673ef40bb2e8900bc227fb352
plonxtoni <antoni.plonczak@gmail.com>
2026-02-19 16:39:25 +0100

    added longitudinal (ends) burning to burning area, chamber volume. added exceptions for non implemented grain types. added burnout function for terminal event in ivp solver

--- core/geometry.py
+++ core/geometry.py
@@ -1,18 +1,32 @@
 import numpy as np
 
+def current_bates_grain_geometry(web, geom):
+    r_i = geom.r_i0 + web       # current inner radius (free volume), increasing due to inner surface burning
+    L   = geom.L0 - 2 * web     # current length, decreasing due to ends burning
+    return r_i, L
+
 def burning_area(web, geom):
-    if geom.grain_type=='bates': #simple cylindrical grain
-        r_i = geom.r_o - web
-        return 2*np.pi*r_i*geom.L
-    #TODO: implement star geometry for comparison experiments
+    if geom.grain_type == 'bates':
+        r_i, L = current_bates_grain_geometry(web, geom)
+        inner_surface = 2 * np.pi * r_i * L             # inner surface burning
+        ends    = 2 * np.pi * (geom.r_o**2 - r_i**2)    # ends burning (annular surfaces)
+        return inner_surface + ends
+    else:
+        NotImplementedError(f"Grain of type '{geom.grain_type}' not implemented")
 
 def chamber_volume(web, geom, plenum=1e-3):
-    if geom.grain_type=='bates': #simple cylindrical grain
-        r_i = geom.r_o - web
-        return plenum + np.pi*geom.r_i**2*geom.L
+    if geom.grain_type == 'bates':
+        r_i, L = current_geometry(web, geom)
+        inner_free_volume = np.pi * r_i**2 * L          # inner free chamber volume
+        ends_volume = 2 * np.pi * geom.r_o**2 * web     # free chamber volume at the ends
+        return plenum + inner_free_volume + ends_volume
+    else:
+        NotImplementedError(f"Grain of type '{geom.grain_type}' not implemented")
 
-def dVdt(web, r_b, geom):
-    return burning_area(web,geom) * r_b # inner area times rate of regression (burn rate)
+def burnout_web(geom):
+    radial_limit = geom.r_o - geom.r_i0     # due to decreasing grain thickness
+    axial_limit  = geom.L0 / 2              # due to decreasing grain length
+    return min(radial_limit, axial_limit)   # burnout occurs when either length or thickness is consumed
 
 def throat_area(nozzle):
-    return np.pi * nozzle.throat_radius**2 
+    return np.pi * nozzle.throat_radius**2
