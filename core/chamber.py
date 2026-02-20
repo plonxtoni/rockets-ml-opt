9aac3d264ce940cc6421335bb7df7baae0bbc123
plonxtoni <antoni.plonczak@gmail.com>
2026-02-20 01:24:57 +0100

    refactor

--- core/chamber.py
+++ core/chamber.py
@@ -3,10 +3,14 @@ from combustion import burn_rate
 from nozzle import characteristic_velocity
 
 def rhs(t, y, propellant, geom, nozzle):      # right hand side of the ode to be solved with scipy ivp
+    # unpack state
+    P_c, web = y
+    
     # propellant characteristics 
     R = propellant.R
     T_c = propellant.T_c
     rho = propellant.rho
+    c_star = characteristic_velocity(propellant, eta_cstar=1)
 
     # geometry calculations
     A_t = throat_area(nozzle)           # throat area
@@ -15,11 +19,8 @@ def rhs(t, y, propellant, geom, nozzle):      # right hand side of the ode to be
     
     # combustion calculation
     r_b = burn_rate(P_c, propellant)    # burn rate (Saint Robert's law)
-    
-    # unpack state
-    P_c, web = y
-    
-    dwebdt = burn_rate(P_c, propellant)
-    dPcdt = ((R*T_c)/V_c) * (rho*A_b*r_b - (A_t/characteristic_velocity(propellant)*P_c))
+
+    dPcdt = ((R*T_c)/V_c) * (rho*A_b*r_b - (A_t*P_c)/c_star)
+    dwebdt = rb
 
     return [dPcdt, dwebdt]
