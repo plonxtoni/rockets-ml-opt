f2135653857d19b7b477dfdbe12bd5a742a27620
plonxtoni <antoni.plonczak@gmail.com>
2026-02-20 02:20:45 +0100

    added imports, fixed typos

--- core/chamber.py
+++ core/chamber.py
@@ -1,26 +1,27 @@
 from geometry import chamber_volume, burning_area, throat_area
 from combustion import burn_rate
 from nozzle import characteristic_velocity
+from config import MotorConfig
 
-def rhs(t, y, propellant, geom, nozzle):      # right hand side of the ode to be solved with scipy ivp
+def rhs(t, y, config: MotorConfig):      # right hand side of the ode to be solved with scipy ivp
     # unpack state
     P_c, web = y
     
     # propellant characteristics 
-    R = propellant.R
-    T_c = propellant.T_c
-    rho = propellant.rho
-    c_star = characteristic_velocity(propellant, eta_cstar=1)
+    R = config.propellant.R
+    T_c = config.propellant.T_c
+    rho = config.propellant.rho
+    c_star = characteristic_velocity(config)
 
     # geometry calculations
-    A_t = throat_area(nozzle)           # throat area
-    A_b = burning_area(web, geom)       # grain burn area
-    V_c = chamber_volume(web, geom)     # free chamber volume
+    A_t = throat_area(config.nozzle)           # throat area
+    A_b = burning_area(web, config.geom)       # grain burn area
+    V_c = chamber_volume(web, config.geom)     # free chamber volume
     
     # combustion calculation
-    r_b = burn_rate(P_c, propellant)    # burn rate (Saint Robert's law)
+    r_b = burn_rate(P_c, config.propellant)    # burn rate (Saint Robert's law)
 
     dPcdt = ((R*T_c)/V_c) * (rho*A_b*r_b - (A_t*P_c)/c_star)
-    dwebdt = rb
+    dwebdt = r_b
 
     return [dPcdt, dwebdt]
